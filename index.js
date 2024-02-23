7fd362c7ba7460d7e57167a0605d03e487ffcb1a
darshan373 <109227678+darshan373@users.noreply.github.com>
2024-02-23 11:58:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+
+const input=document.getElementById("input");
+const cf=document.getElementById("tofaran");
+const fc=document.getElementById("tocelcius");
+const result=document.getElementById("res");
+   
+
+const submitButton=document.querySelector("#submit");
+submitButton.onclick=function(){
+    if(cf.checked)
+    {
+        let val=input.value;
+        val=Number(val);
+        val= (val*9/5)+32;
+        result.textContent=val+"°F"
+    }
+    else if(fc.checked)
+    {
+        let val=input.value;
+        val=Number(val);
+        val= (val-32)*5/9;
+        result.textContent=val+"°C";
+
+
+    }
+    else{
+        result.textContent="select a unit";
+    }
+
+}
+
+
+/*
+function convertor()
+{
+    if(cf.checked)
+    {
+        let val=input.value;
+        val=Number(val);
+        val= (val*9/5)+32;
+        result.textContent(val+"°F");
+    }
+    else if(fc.checked)
+    {
+        let val=input.value;
+        val=Number(val);
+        val= (val-32)*5/9;
+        result.textContent(val+"°C");
+
+
+    }
+    else{
+        result.textContent("select a unit")
+    }
+}*/
